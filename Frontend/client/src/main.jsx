f3eb63bd0e22ff036705eca3b6af7804a1d67b5c
esther-malowa <malowaesther11@gmail.com>
2025-11-15 16:45:55 +0300

    final commit

--- Frontend/client/src/main.jsx
+++ Frontend/client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById('root')).render(
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  tracesSampleRate: 1.0, 
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
